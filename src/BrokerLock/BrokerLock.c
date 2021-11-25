6c72f09aa14197317a57cab7775a51047a6a0982
jrachucki <jrachucki@n7space.com>
2021-11-25 16:21:46 +0100

    Broker lock samv71 implementation provided

---
+++ src/BrokerLock/BrokerLock.c
@@ -0,0 +1,51 @@
+/**@file
+ * This file is part of the TASTE Runtime Common.
+ *
+ * @copyright 2021 N7 Space Sp. z o.o.
+ *
+ * TASTE Runtime Common was developed under a programme of,
+ * and funded by, the European Space Agency (the "ESA").
+ *
+ * Licensed under the ESA Public License (ESA-PL) Permissive,
+ * Version 2.3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://essr.esa.int/license/list
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <BrokerLock.h>
+
+#include <FreeRTOS.h>
+#include <semphr.h>
+
+static SemaphoreHandle_t broker_lock = { NULL };
+
+static inline void
+Broker_create_and_init_lock()
+{
+    if(broker_lock == NULL) {
+        broker_lock = xSemaphoreCreateBinary();
+        xSemaphoreGive(broker_lock);
+    }
+}
+
+void
+Broker_acquire_lock()
+{
+    Broker_create_and_init_lock();
+    xSemaphoreTake(broker_lock, portMAX_DELAY);
+}
+
+void
+Broker_release_lock()
+{
+    Broker_create_and_init_lock();
+    xSemaphoreGive(broker_lock);
+}
---
+++ src/BrokerLock/BrokerLock.h
@@ -0,0 +1,36 @@
+/**@file
+ * This file is part of the TASTE Runtime Common.
+ *
+ * @copyright 2021 N7 Space Sp. z o.o.
+ *
+ * TASTE Runtime Common was developed under a programme of,
+ * and funded by, the European Space Agency (the "ESA").
+ *
+ * Licensed under the ESA Public License (ESA-PL) Permissive,
+ * Version 2.3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://essr.esa.int/license/list
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef BROKER_LOCK_H
+#define BROKER_LOCK_H
+
+/**
+ * @file    BrockerLock.h
+ * @brief   Headers for lock functions required by Broker.
+ */
+
+/// @brief function used by Broker to acquire the lock.
+void Broker_acquire_lock();
+/// @brief function used by Broker to release the lock.
+void Broker_release_lock();
+
+#endif
