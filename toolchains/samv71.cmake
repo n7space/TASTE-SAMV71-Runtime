3f91db59c85571d74fc83ff0d25cc3b846c5d56f
Filip Demski <glamhoth@protonmail.com>
2021-11-25 14:19:43 +0100

    Setting path to the linker script just for the FreeRTOSApp

--- toolchains/samv71.cmake
+++ toolchains/samv71.cmake
@@ -6,10 +6,6 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
 
-if(${SAMV71_BSP_DIR})
-message(FATAL_ERROR "SAMV71_BSP_DIR not defined")
-endif()
-
 set(CMAKE_C_FLAGS
 "-mcpu=cortex-m7 \
 -mfloat-abi=hard \
@@ -28,5 +24,4 @@ set(CMAKE_EXE_LINKER_FLAGS
 "-eexception_table \
 -Wl,-Map=output.map \
 -Wl,--gc-sections \
--specs=nosys.specs \
--T${SAMV71_BSP_DIR}/ld/samv71q21_sram.ld")
+-specs=nosys.specs")
