493be5d0d31c417cf3a46e18bfebed579aaa1c82
jrachucki <jrachucki@n7space.com>
2022-01-19 14:46:14 +0100

    License added

--- src/uartXdmacDemo/main.c
+++ src/uartXdmacDemo/main.c
@@ -1,3 +1,25 @@
+/**@file
+ * This file is part of the TASTE Runtime SAMV71.
+ *
+ * @copyright 2021 N7 Space Sp. z o.o.
+ *
+ * TASTE Runtime SAMV71 was developed under a programme of,
+ * and funded by, the European Space Agency (the "ESA").
+ *
+ * Licensed under the ESA Public License (ESA-PL) Permissive,
+ * Version 2.3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://essr.esa.int/license/list
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
